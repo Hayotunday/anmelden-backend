37737afc4c17f7a61ca3a2ba8fe81e84d64f57f7
Hayotunday <idowudanielayotunde@gmail.com>
2023-04-24 20:58:58 +0100

    Added app.js sent to me

--- app.js
+++ app.js
@@ -1,264 +1,3 @@
-// const express = require('express')
-// const mysql = require('mysql')
-// const bodyParser = require('body-parser');
-// const nodemailer = require('nodemailer')
-// const cors = require('cors')
-// require('dotenv').config()
-
-// const app = express();
-// app.use(cors({
-//   origin: '*'
-// }));
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-// app.use(bodyParser.json());
-
-// const host = process.env.DATABASEHOST
-// const user = process.env.DATABASEUSER
-// const password = process.env.DATABASEPASSWORD
-// const email_register = process.env.EMAILREGISTER
-// const mailpass_register = process.env.MAILPASSREGISTER
-// const email_entry = process.env.EMAILENTRY
-// const mailpass_entry = process.env.MAILPASSENTRY
-
-// // Create connection
-// const db = mysql.createConnection({
-//   host: host,
-//   user: user,
-//   password: password,
-//   database: user
-// })
-
-// db.connect((err) => {
-//   if (err) {
-//     console.log('MySql not connected...')
-//     throw err
-//   }
-//   console.log('MySql Connected...')
-// })
-
-
-// // Mail sender function
-// const sendMail = async (receiver, name, type) => {
-//   let transporter
-//   let mailOptions = {}
-
-//   switch (type) {
-//     case "entry":
-//       transporter = nodemailer.createTransport({
-//         service: 'gmail',
-//         host: "smtp.gmail.com",
-//         port: 465,
-//         secure: true,
-//         auth: {
-//           user: email_entry,
-//           pass: mailpass_entry
-//         }
-//       });
-
-//       mailOptions = {
-//         from: email_entry,
-//         to: receiver,
-//         cc: ["sekretariat@swissalbmed"],
-//         subject: 'Willkommen in unserer Gemeinschaft!',
-//         html: `<div>
-//         <p>
-//         Sehr geehrtes, ${name}
-//         </p>
-
-//         <P>
-//           Wir freuen uns sehr, dass Sie der Gemeinschaft von Swissalbmed beigetreten sind! Ihre Mitgliedschaft bedeutet uns sehr viel, und wir sind bestrebt, hnen die Ressourcen, die Unterstützung und die Vorteile zu bieten, die Sie für Ihren Erfolg benötigen.
-//         </P>
-
-//         <p>
-//           Als Mitglied haben Sie Zugang zu exklusiven Veranstaltungen, Online-Ressourcen usw. Wir ermutigen Sie, alle Vorteile der Mitgliedschaft zu nutzen  und sich in unserer Gemeinschaft zu engagieren.
-//         </p>
-
-//         <p>
-//           Wir freuen uns darauf, mit Ihnen zusammenzuarbeiten und Sie auf Ihrem Weg zu unterstützen. Wenn Sie Fragen haben oder Hilfe benötigen, zögern Sie  bitte nicht, sich an uns zu wenden.
-//         </p>
-
-//         <p>
-//           Mit freundlichen Grüßen,
-//           Valbona MIftari
-//         </p>
-
-//         <p>
-//           Dr. med. Valbona Miftari
-//         </p>
-
-//         <p>
-//           Swissalbmed
-//         </p>
-//       </div>`
-//       };
-
-//       transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//           console.log('Email not sent: ' + error);
-//         } else {
-//           console.log('Email sent: ' + info.response);
-//         }
-//       })
-//       break;
-//     case "register":
-//       transporter = nodemailer.createTransport({
-//         service: 'gmail',
-//         host: "smtp.gmail.com",
-//         port: 465,
-//         secure: true,
-//         auth: {
-//           user: email_register,
-//           pass: mailpass_register
-//         }
-//       });
-
-//       mailOptions = {
-//         from: email_register,
-//         to: receiver,
-//         cc: ["sekretariat@swissalbmed"],
-//         subject: 'Willkommen zum Symposium!',
-//         html: `<div>
-//         <p>
-//           Sehr geehrte(r) ${name}
-//         </p>
-
-//         <P>
-//           wir freuen uns sehr, dass Sie am Anmeldung symposium teilnehmen werden! Wir haben hart daran gearbeitet, ein
-//           unvergessliches Erlebnis für alle Teilnehmer zu schaffen, und wir können es kaum erwarten, dass Sie ein Teil davon
-//           sind.
-//         </P>
-
-//         <p>
-//           Auf dem Symposium werden Sie die Möglichkeit haben, [einige der wichtigsten Höhepunkte der Veranstaltung
-//           aufzulisten, wie z. B. Networking-Möglichkeiten, Vorträge von Gastrednern, Podiumsdiskussionen usw.]. Unser Ziel ist
-//           es, Ihnen wertvolle Einblicke aktuelle Informationen etc zu vermitteln, die Sie in Ihr [berufliches/persönliches
-//           leben mitnehmen und anwenden können.
-//         </p>
-
-//         <p>
-//           Bitte zögern Sie nicht, uns zu kontaktieren, wenn Sie Fragen oder Bedenken haben. Unser Team steht Ihnen während des
-//           gesamten Anmeldung symposium zur Verfügung.
-//         </p>
-
-//         <p>
-//           Noch einmal: Herzlich willkommen zum Anmeldung symposium. Wir freuen uns darauf, Sie bald wiederzusehen!
-//         </p>
-
-//         <p>
-//           Mit freundlichen Grüssen,
-//         </p>
-
-//         <p>
-//           Dr. med. Valbona Miftari
-//         </p>
-
-//         <p>
-//           Swissalbmed
-//         </p>
-//       </div>`
-//       };
-
-//       transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//           console.log('Email not sent: ' + error);
-//         } else {
-//           console.log('Email sent: ' + info.response);
-//         }
-//       })
-//       break;
-//     default:
-//       break;
-//   }
-// }
-
-// // Insert details to registration table
-// app.post('/addnewdetails/', (req, res) => {
-//   const { firstname, lastname, email, phone, profession } = req.body;
-//   sql = `insert into Registration (firstname, lastname, email, phone, profession) values ('${firstname}','${lastname}','${email}','${phone}','${profession}')`;
-//   db.query(sql, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(result)
-//     res.send('Inserted into database.....');
-//     const type = "register"
-//     sendMail(email, firstname, type);
-//   })
-// })
-
-// // Insert data to entry table
-// app.post('/addentry/', (req, res) => {
-//   const {
-//     salutation,
-//     employment,
-//     title,
-//     practice,
-//     eduTitle,
-//     street,
-//     profession,
-//     postcode,
-//     firstname,
-//     location,
-//     lastname,
-//     phone,
-//     dob,
-//     email,
-//     diplomaCountry,
-//     privateaddress,
-//     diplomayear,
-//     privatezip,
-//     gln,
-//     privatelocation,
-//     uid,
-//     privatephone
-//   } = req.body;
-
-//   sql = `insert into EntryFormTable (
-//     salutation,
-//     employment,
-//     title,
-//     practice,
-//     eduTitle,
-//     street,
-//     profession,
-//     postcode,
-//     firstname,
-//     location,
-//     surname,
-//     phone,
-//     dob,
-//     email,
-//     diplomaCountry,
-//     privateaddress,
-//     diplomayear,
-//     privatezip,
-//     gln,
-//     privatelocation,
-//     uid,
-//     privatephone
-//     ) values ('${salutation}','${employment}','${title}','${practice}','${eduTitle}','${street}','${profession}','${postcode}','${firstname}','${location}','${lastname}','${phone}','${dob}','${email}','${diplomaCountry}','${privateaddress}','${diplomayear}','${privatezip}','${gln}','${privatelocation}','${uid}','${privatephone}')`;
-//   db.query(sql, (err, result) => {
-//     if (err) {
-//       console.log(err);
-//     }
-//     console.log(result)
-//     res.send('Inserted into database.....');
-//     const type = "entry";
-//     sendMail(email, firstname, type);
-//   })
-// })
-
-// const port = process.env.PORT || 3000
-
-// app.listen(port, () => {
-//   console.log('Server started on port 3000')
-// })
-
-
 const express = require('express')
 const mysql = require('mysql')
 const bodyParser = require('body-parser');
@@ -323,7 +62,7 @@ const sendMail = async (receiver, name, type) => {
       mailOptions = {
         from: email_entry,
         to: receiver,
-        cc: "sekretariat@swissalbmed.ch",
+        cc: ["sekretariat@swissalbmed.ch"],
         subject: 'Willkommen in unserer Gemeinschaft!',
         html: `<div>
         <p>
@@ -380,42 +119,39 @@ const sendMail = async (receiver, name, type) => {
       mailOptions = {
         from: email_register,
         to: receiver,
-        cc: "sekretariat@swissalbmed.ch",
-        subject: 'Danke für die Anmeldung!',
+        cc: ["sekretariat@swissalbmed.ch"],
+        subject: 'Faleminderit për regjistrim!',
         html: `<div>
         <p>
-          Guten Tag ${name}
+          E/I nderuar/i ${name}
         </p>
       
         <P>
-          wir freuen uns sehr, dass Sie am symposium teilnehmen werden! Wir haben hart daran gearbeitet, ein
-          unvergessliches Erlebnis für alle Teilnehmer zu schaffen, und wir können es kaum erwarten, dass Sie ein Teil davon
-          sind.
+          Me kënaqësi iu konfirmoj anëtarësimin tuaj në Lidhjen e Mjekëve Shqiptarë në Zvicër dhe përzemërsisht iu dëshirojmë mirëseardhje!
         </P>
       
         <p>
-          Auf dem Symposium werden Sie die Möglichkeit haben, wie z. B. Networking-Möglichkeiten, Vorträge von Gastrednern, Podiumsdiskussionen usw.]. Unser Ziel ist es, Ihnen wertvolle Einblicke aktuelle Informationen etc zu vermitteln, die Sie in Ihr berufliches leben mitnehmen und anwenden können.
+          Në mënyrë që të njiheni për së afërmi me LMSHZ-në, gjegjësisht me misionin e saj, iu ftoj ta vizitoni webfaqen tonë; swissalbmed.ch
         </p>
       
         <p>
-          Bitte zögern Sie nicht, uns zu kontaktieren, wenn Sie Fragen oder Bedenken haben. Unser Team steht Ihnen während des
-          gesamten symposium zur Verfügung.
+          Në përputhje me statutin e shoqatës, çdo anëtar është i detyruar të paguajë kuotën e anëtarësimit. Shuma e kontributit vjetor është përcaktuar dhe miratuar në Mbledhjen e Parë të Përgjithshme të Kuvendit. Fletëpagesa e anëtarësisë dërgohet në fillim të vitit.
         </p>
       
         <p>
-          Noch einmal: Herzlich willkommen zum symposium. Wir freuen uns darauf, Sie bald wiederzusehen!
+          Për çdo pyetje dhe paqartësi ju lutem t’na kontaktoni nëpermes E-Mailit: sekretariat@swissalbmed.ch
         </p>
       
         <p>
-          Mit freundlichen Grüssen,
+          Me respekt!
         </p>
       
         <p>
-          Dr. med. Valbona Miftari
+          Dr. med. Valbone Miftari
         </p>
       
         <p>
-          Swissalbmed
+          Sekretare e përgjithshme e LMSHZ-së
         </p>
       </div>`
       };
